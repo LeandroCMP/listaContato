120172c792b7b50bdd6656ff13b727f19c31c611
LeandroCampos <Ins4neHS@gmail.com>
2019-04-18 16:59:56 -0300

    Lista de Contatos Flutter

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:lista_contatos/ui/contact_page.dart';
+import 'package:lista_contatos/ui/home_page.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+
+}
